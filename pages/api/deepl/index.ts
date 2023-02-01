f6bfc513ecd751805781fcc461648a66da49a2b7
zorro901 <zoro907@gmail.com>
2023-02-01 11:28:59 +0900

    リファクタリング

--- pages/api/deepl/index.ts
+++ pages/api/deepl/index.ts
@@ -1,47 +1,75 @@
-// For Local Environment
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const sliceByNumber = (array: string | any[], number: number) => {
-  const length = Math.ceil(array.length / number)
-  // @ts-ignore
-  return new Array(length).fill().map((_, i) => array.slice(i * number, (i + 1) * number))
-}
-const delay = (ms: number | undefined) => new Promise(resolve => setTimeout(resolve, ms))
-
 //モジュールを読み込み
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const chromium = require('playwright-aws-lambda')
-//Puppeteerを実行
+import chromium from 'playwright-aws-lambda'
+
+const sliceByNumber = (array: string[], number: number): string[][] => {
+  const length = Math.ceil(array.length / number)
+  return new Array(length).fill(undefined).map((_, i) => array.slice(i * number, (i + 1) * number))
+}
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+//半角記号を全角へ変換
+const toZenWidth = (strVal: string) => {
+  const zenVal = strVal.replaceAll(/[<>!@#$%^&*)(+=._-]/g, (tmpStr: string) =>
+    String.fromCharCode(tmpStr.charCodeAt(0) + 0xfee0)
+  )
+  // 文字コードシフトで対応できない文字の変換
+  return zenVal.replace(/"/g, '”').replace(/'/g, '’').replace(/`/g, '‘').replace(/\\/g, '￥').replace(/~/g, '〜')
+}
+//Playwrightを実行
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  //個別パラメータを取得する
-  const text = req.body.text
+  //JSONリクエストパラメータを取得する
+  const wordObject: Record<string, string> = JSON.parse(req.body)
+  const wordlist = []
+  for (const [, value] of Object.entries(wordObject)) {
+    wordlist.push(`${toZenWidth(value)}`)
+  }
+  const text = wordlist.join('\n')
   //Puppeteerを準備する
   //高速化の為に余計なオプションはオフにしておく
   const browser = await chromium.launchChromium({
     args: [
-      '--no-sandbox',
+      '--allow-running-insecure-content', // https://source.chromium.org/search?q=lang:cpp+symbol:kAllowRunningInsecureContent&ss=chromium
+      '--autoplay-policy=user-gesture-required', // https://source.chromium.org/search?q=lang:cpp+symbol:kAutoplayPolicy&ss=chromium
+      '--disable-component-update', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableComponentUpdate&ss=chromium
+      '--disable-domain-reliability', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableDomainReliability&ss=chromium
+      '--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process', // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+      '--disable-print-preview', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisablePrintPreview&ss=chromium
+      '--disable-setuid-sandbox', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSetuidSandbox&ss=chromium
+      '--disable-site-isolation-trials', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSiteIsolation&ss=chromium
+      '--disable-speech-api', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSpeechAPI&ss=chromium
+      '--disable-web-security', // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableWebSecurity&ss=chromium
+      '--disk-cache-size=33554432', // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
+      '--enable-features=SharedArrayBuffer', // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+      '--hide-scrollbars', // https://source.chromium.org/search?q=lang:cpp+symbol:kHideScrollbars&ss=chromium
+      '--ignore-gpu-blocklist', // https://source.chromium.org/search?q=lang:cpp+symbol:kIgnoreGpuBlocklist&ss=chromium
+      '--in-process-gpu', // https://source.chromium.org/search?q=lang:cpp+symbol:kInProcessGPU&ss=chromium
+      '--mute-audio', // https://source.chromium.org/search?q=lang:cpp+symbol:kMuteAudio&ss=chromium
+      '--no-default-browser-check', // https://source.chromium.org/search?q=lang:cpp+symbol:kNoDefaultBrowserCheck&ss=chromium
+      '--no-pings', // https://source.chromium.org/search?q=lang:cpp+symbol:kNoPings&ss=chromium
+      '--no-sandbox', // https://source.chromium.org/search?q=lang:cpp+symbol:kNoSandbox&ss=chromium
+      '--no-zygote', // https://source.chromium.org/search?q=lang:cpp+symbol:kNoZygote&ss=chromium
+      '--use-gl=swiftshader', // https://source.chromium.org/search?q=lang:cpp+symbol:kUseGl&ss=chromium
+      '--window-size=1920,1080', // https://source.chromium.org/search?q=lang:cpp+symbol:kWindowSize&ss=chromium
+
+      //追加オプション
       '--disable-gpu',
       '--disable-dev-shm-usage',
-      '--disable-setuid-sandbox',
-      '--no-first-run',
-      '--no-zygote',
       '--single-process',
-      //追加オプション
       '--disable-background-networking',
       '--disable-default-apps',
       '--disable-extensions',
       '--disable-sync',
       '--disable-translate',
-      '--hide-scrollbars',
       '--metrics-recording-only',
-      '--mute-audio',
       '--no-first-run',
       '--safebrowsing-disable-auto-update',
       '--ignore-certificate-errors',
       '--ignore-ssl-errors',
       '--ignore-certificate-errors-spki-list'
     ],
-    headless: true
+    headless: false
   })
   const context = await browser.newContext()
   const page = await context.newPage()
@@ -49,32 +77,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   await page.setExtraHTTPHeaders({
     'Accept-Language': 'ja-JP'
   })
+  let langSetting = ''
+  req.body.original && req.body.exchange ? (langSetting = `#${req.body.original}/${req.body.exchange}/`) : null
+
   //ページを開く
-  await page.goto('https://www.deepl.com/translator')
+  await page.goto(`https://www.deepl.com/translator${langSetting}`)
+
   // 改行した数を取得
   const indentionNumber = text.split('\n').length // 60
   // 文字数単位の空配列を生成する
-  const splitStringArray = Array(Math.ceil(text.length / (5000 - indentionNumber)))
+  const splitStringArray = Array(Math.ceil(text.length / 2900))
   // 文章を配列に分割して再格納
   const splitArrayNumber = sliceByNumber(text.split('\n'), indentionNumber / splitStringArray.length)
 
-  const returnArr: string[][] = []
+  const resultArray = []
   for (let i = 0; i < splitArrayNumber.length; i++) {
     //翻訳元の言葉を入力
-    // @ts-ignore
     await page.fill('textarea', splitArrayNumber[i].join('\n'))
-
     //翻訳を待つ
     await page.waitForSelector('.lmt--active_translation_request', { state: 'hidden' })
     //翻訳結果を取得する
     const targetSentenceField = '.lmt__target_textarea'
 
-    const result = {}
-    // @ts-ignore
-    result.target = await page.$eval(targetSentenceField, (el: { value: any }) => el.value)
+    const result: { target: string } = { target: '' }
+    result.target = await page.$eval(targetSentenceField, (el: HTMLTextAreaElement) => el.value)
 
     //改行された場合は分割する
-    // @ts-ignore
     const arr = result.target.split(/\r\n|\n/)
     //分割した配列へ整形する
     const arrSplit = () => {
@@ -84,12 +112,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
     //整形したデータを配列へ再格納
     const arrComp = arrSplit()
-    // console.log(arrComp)
-    returnArr.push(arrComp)
+    resultArray.push(arrComp)
     splitArrayNumber.length >= 2 ? await delay(10000) : null
   }
 
-  res.status(200).json({ body: returnArr.flat() })
-
   await browser.close()
+  const split = resultArray.flat()
+  for (const [key, value] of Object.entries(wordObject)) {
+    const nlCount = (value.match(/\n/g) || []).length
+    const word = []
+    for (let i = 0; i <= nlCount; i++) {
+      word.push(split[0])
+      split.shift()
+    }
+    wordObject[key] = word.join('\n')
+  }
+  res.status(200).json({ body: wordObject })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,109 @@
-import axios from 'axios'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import type { ChangeEvent } from 'react'
 
-async function translateDeepL(arr: string[]) {
-  const translateFinArray: string[][] = []
-  for (const currentPromise of arr) {
-    const retryRequest = async (count = 0) => {
+const translateDeepL = async (targetStringArray: string[]): Promise<Record<'text', string>> => {
+  const translateFinArray: Record<'text', string>[] = []
+  for (const currentPromise of targetStringArray) {
+    const retryRequest = async (retryCount = 0) => {
       try {
-        // const res = await axios.post('/api/deepl', {
-        const res = await axios.post(`${process.env.TRANSLATE_URL}`, {
-          text: currentPromise
-        })
-        translateFinArray.push(res.data.body)
+        const response = await fetch('api/deepl', { method: 'POST', body: JSON.stringify({ text: currentPromise }) })
+        if (!response.ok) return null
+        const json = await response.json()
+        translateFinArray.push(json.body)
       } catch (error) {
-        if (count === 4) {
-          return null
-        }
-        await retryRequest(count + 1)
+        if (retryCount === 4) return null
+        await retryRequest(retryCount + 1)
       }
     }
     await retryRequest()
   }
-  return translateFinArray
+  return translateFinArray[0]
 }
-// @typescript-eslint/no-explicit-any
-const srtTranslate = async (file: any) => {
-  return new Promise(resolve => {
-    const reader = new FileReader()
-    reader.readAsText(file)
-    return (reader.onload = async evt => {
-      // 字幕を展開する
-      const loadsub_: string = evt.target?.result as string
+const srtTranslate = async (srtFile: File) => {
+  const reader = new FileReader()
+  reader.readAsText(srtFile)
+  reader.onload = async readerEvent => {
+    // 字幕を展開する
+    const targetSubtitle = readerEvent.target?.result
+    if (typeof targetSubtitle !== 'string') return
+    const originalSubtitle: Array<string> = targetSubtitle.replace(/\r/g, '').split('\n')
 
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const loadsub: Array<string> = loadsub_.replace(/\r/g, '').split('\n')!
-      const testIndex: number[] = []
-      const test = loadsub.map((v, i) => {
-        if (RegExp(`^\\d{1,3}`, 'g').test(v) || v === '') {
-          return null
-        } else {
-          testIndex.push(i)
-          return v
-        }
-        // return RegExp(`^\\d{1,3}`, 'g').test(v) || v === '' ? null : v && testIndex.push(i)
-      })
-      const sub = test.filter(Boolean).join('\n')
-      // 翻訳後の字幕を格納する配列
-      const translateFinArray: string[][] = await translateDeepL([sub])
-      // 字幕データを翻訳する
-
-      // 翻訳した字幕を結合する
-      const sub_trans_data = translateFinArray[0].flat()
-      const mixedSub: string[] = []
-
-      loadsub.map((v, i) => {
-        if (i === testIndex[0]) {
-          mixedSub.push(sub_trans_data[0])
-          sub_trans_data.shift()
-          testIndex.shift()
-        } else {
-          mixedSub.push(v)
-        }
-      })
-      // 拡張子を取得
-      const file_type = file.name.split('.').pop()
-
-      // 完成したデータを自動ダウンロード
-      const blob = new Blob([mixedSub.join('\n')], { type: 'text/plain' })
-      const url = URL.createObjectURL(blob)
-      const a = document.createElement('a')
-      document.body.appendChild(a)
-      a.download = `${file.name.match(/(.*)\.([^.]+$)/)[1]}_ja.${file_type}`
-      a.href = url
-      a.click()
-      a.remove()
-      URL.revokeObjectURL(url)
-
-      resolve('end')
+    // 字幕の並び順と内容を配列に変換する
+    const contentArrayIndex: number[] = []
+    const contentTextArray = originalSubtitle.map((text, index) => {
+      if (RegExp(`^\\d{1,3}`, 'g').test(text) || text === '') {
+        return null
+      } else {
+        contentArrayIndex.push(index)
+        return text
+      }
     })
-  })
+
+    const beforeTranslateText = contentTextArray.filter(Boolean).join('\n')
+    // 字幕データを翻訳する
+
+    const translatedText = await translateDeepL([beforeTranslateText])
+    // 翻訳した字幕を結合する
+    const translatedTextArray = translatedText.text.split('\n')
+    const finishedSubtitleArray: string[] = []
+
+    originalSubtitle.map((v, i) => {
+      if (i === contentArrayIndex[0]) {
+        finishedSubtitleArray.push(translatedTextArray[0])
+        translatedTextArray.shift()
+        contentArrayIndex.shift()
+      } else {
+        finishedSubtitleArray.push(v)
+      }
+    })
+
+    // 拡張子を取得
+    const fileName = srtFile.name
+    const file_type = fileName.split('.').pop()
+
+    // 完成したデータを自動ダウンロード
+    const blob = new Blob([finishedSubtitleArray.join('\n')], { type: 'text/plain' })
+    const url = URL.createObjectURL(blob)
+    const anchorElement = document.createElement('a')
+    document.body.appendChild(anchorElement)
+
+    // ファイル名に_jaを追加する
+    let defaultName = fileName.match(/(.*)\.([^.]+$)/)?.[1]
+    if (defaultName === undefined) return
+    if (defaultName.indexOf('_en') !== -1) {
+      defaultName = defaultName.slice(0, -3)
+    }
+    if (defaultName.indexOf('en_US') !== -1) {
+      defaultName = defaultName.slice(0, -5)
+    }
+    anchorElement.download = `${defaultName}_ja.${file_type}`
+    anchorElement.href = url
+    anchorElement.click()
+    anchorElement.remove()
+    URL.revokeObjectURL(url)
+  }
 }
 const Home: NextPage = () => {
-  const handleMultiUploadFile = async (event: any) => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const loading = document.getElementById('loading')!
-    loading.style.display = 'flex'
+  const handleMultiUploadFile = async (event: ChangeEvent<HTMLInputElement>) => {
+    const loading = document.getElementById('loading')
+    const upload = document.getElementById('upload')
+    if (!loading || !upload) return
+    loading.classList.remove('hidden')
+    upload.classList.add('hidden')
 
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const upload = document.getElementById('upload')!
-    upload.style.display = 'none'
-
-    const FileList: FileList = event.target.files
+    const FileList = event.target.files
+    if (!FileList) return
     const files = Array.from(FileList)
 
     for (let i = 0; i < files.length; i++) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const result = await srtTranslate(files[i])
+      await srtTranslate(files[i])
+      if (i === files.length - 1) {
+        loading.classList.add('hidden')
+        upload.classList.remove('hidden')
+      }
     }
-    upload.style.display = 'flex'
-    loading.style.display = 'none'
   }
-  // @ts-ignore
   return (
     <>
       <Head>
@@ -108,19 +111,25 @@ const Home: NextPage = () => {
         <meta name='description' content='SRT TRANSLATOR' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className={'container bg-gray-600 flex  w-full h-screen items-center flex-col  py-16'}>
+      <div className={'bg-gray-600 flex w-screen h-screen items-center flex-col pt-16'}>
         <div className={'container flex flex-col'}>
           <h4 className={'flex justify-center font-bold text-white text-lg'}>SRT TRANSLATOR</h4>
         </div>
 
         <div className={'container flex flex-col w-full items-center py-16'}>
-          <div className='bg-grey-lighter ' id={'upload'}>
+          <div className='bg-grey-lighter' id={'upload'}>
             <label className='w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue-100 hover:text-black'>
               <svg className='w-8 h-8' fill='currentColor' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
                 <path d='M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z' />
               </svg>
               <span className='mt-2 text-base leading-normal'>Select a file</span>
-              <input type='file' className='hidden' onChange={handleMultiUploadFile} accept='.srt,.vtt' multiple />
+              <input
+                type='file'
+                className='hidden'
+                onChange={event => handleMultiUploadFile(event)}
+                accept='.srt,.vtt'
+                multiple
+              />
             </label>
           </div>
 
