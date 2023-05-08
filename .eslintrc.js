df579dc6fe1046d40f4fe989147c1bc7dc461b96
zorro901 <zoro907@gmail.com>
2023-05-08 16:41:04 +0900

    eslintの設定を変更

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSpacing: true,
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'none',
+        semi: false,
+        parser: 'typescript'
+      }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    'spaced-comment': ['error', 'always', { exceptions: ['-'] }]
+  }
+}
